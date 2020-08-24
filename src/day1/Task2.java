2bd5bc549661db02def18edb70f7bd6113e3bc16
Elena Bakhareva <sentidas@yandex.ru>
2020-08-24 20:31:11 +0300

    Add day1_tasks

---
+++ src/day1/Task2.java
@@ -0,0 +1,13 @@
+package day1;
+
+public class Task2 {
+  public static void main(String[] args) {
+
+    String word = "JAVA";
+
+    for (int i = 0; i < 10; i++) {
+      System.out.print(word);
+      System.out.print(" ");
+    }
+  }
+}
