65275c13a9d246279e2b551e787108cafc445bb5
Elena Bakhareva <sentidas@yandex.ru>
2020-08-29 01:14:43 +0300

    add day5_tasks

---
+++ src/day5/Task1.java
@@ -0,0 +1,42 @@
+package day5;
+
+import java.sql.SQLOutput;
+
+public class Task1 {
+  public static void main(String[] args) {
+
+    Car car = new Car();
+    car.setYear(1990);
+    car.setColor("red");
+    car.setModel("V32");
+
+    System.out.println("Ты купил машину " + car.getYear() + "-ого года выпуска, цвета " + car.getColor() + ", " + car.getModel() + " - модель");
+  }
+}
+
+
+class Car {
+  int year;
+  String color;
+  String model;
+
+  public void setYear(int year) {
+    this.year = year;
+  }
+  public void setColor(String color) {
+    this.color = color;
+  }
+  public void setModel(String model) {
+    this.model = model;
+  }
+
+  public int getYear() {
+    return year;
+  }
+  public String getColor() {
+    return color;
+  }
+  public String getModel() {
+    return model;
+  }
+}
---
+++ src/day5/Task2.java
@@ -0,0 +1,28 @@
+package day5;
+
+public class Task2 {
+  public static void main(String[] args) {
+
+     Motorbike myMotorbike = new Motorbike (2020, "green", "U456");
+     Motorbike yourMotorbike = new Motorbike (2021, "red");
+
+  }
+}
+
+class Motorbike {
+
+  int year;
+  String color;
+  String model;
+
+  public Motorbike (int year, String color, String model) {
+    this.year = year;
+    this.color = color;
+    this.model = model;
+  }
+
+  public Motorbike (int year, String color) {
+    this.year = year;
+    this.color = color;
+  }
+}
