2b42b580fd269695582de41e90c0b768726039d9
Elena Bakhareva <sentidas@yandex.ru>
2020-08-29 22:04:26 +0300

    change day5_tasks

--- src/day5/Task2.java
+++ src/day5/Task2.java
@@ -6,23 +6,10 @@ public class Task2 {
      Motorbike myMotorbike = new Motorbike (2020, "green", "U456");
      Motorbike yourMotorbike = new Motorbike (2021, "red");
 
+    System.out.println("Ты купил мотоцикл " + yourMotorbike.getYear() + "-ого года выпуска, цвета " + yourMotorbike.getColor());
+
+    System.out.println("Я купил мотоцикл " + myMotorbike.getYear() + "-ого года выпуска, цвета " + myMotorbike.getColor() +
+            ", " + myMotorbike.getModel() + " - модель");
   }
 }
 
-class Motorbike {
-
-  private int year;
-  private String color;
-  private String model;
-
-  public Motorbike (int year, String color, String model) {
-    this.year = year;
-    this.color = color;
-    this.model = model;
-  }
-
-  public Motorbike (int year, String color) {
-    this.year = year;
-    this.color = color;
-  }
-}
